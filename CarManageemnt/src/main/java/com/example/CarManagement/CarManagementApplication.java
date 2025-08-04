ce5791258e34f6607815101ff961e0b342004e29
raees78691 <mohammed-raees.abdus-samad-shaikh@capgemini.com>
2025-08-04 22:49:59 +0530

    format: remove unnecessary blank line for improved readability in CarManagementApplication

--- CarManageemnt/src/main/java/com/example/CarManagement/CarManagementApplication.java
+++ CarManageemnt/src/main/java/com/example/CarManagement/CarManagementApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarManagementApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CarManagementApplication.class, args);
 			}
 
