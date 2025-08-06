146b435ce1b0fd3a81c9a79821568797e0a801fe
raees78691 <mohammed-raees.abdus-samad-shaikh@capgemini.com>
2025-08-06 14:10:04 +0530

    format: add logging configuration and dependencies for improved logging functionality

--- CarManageemnt/src/main/java/com/example/CarManagement/CarManagementApplication.java
+++ CarManageemnt/src/main/java/com/example/CarManagement/CarManagementApplication.java
@@ -1,13 +1,27 @@
 package com.example.CarManagement;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@Log4j2
 @SpringBootApplication
+//@RestController
+//@RequestMapping("/logs")
+
 public class CarManagementApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarManagementApplication.class, args);
-			}
+		//logger.info("This is an info message slf4j.");
+		log.info("Performing some operation.");
+		log.error("An error occurred!", new RuntimeException("Example exception"));
+		log.info("Performing some operation after Exception");
+	}
 
 }
